21e1ba743cca08d71297a0b057c973f4f02a119d
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-11-24 10:21:22 +0100

    Edit background color app

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import SongBar from './SongBar';
 
-const RelatedSongs = ({ data, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
+const RelatedSongs = ({
+  data,
+  artistId,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
   <div className="flex flex-col">
-    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    <h1 className="font-bold text-3xl text-white">Musiques similaires</h1>
 
     <div className="mt-6 w-full flex flex-col">
       {data?.map((song, i) => (
